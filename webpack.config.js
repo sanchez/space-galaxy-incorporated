43150f9ccce70e0e64b6f4fa69acf5f04cabe688
Daniel Fitz <dfitz360@gmail.com>
2019-04-24 09:02:40 +1000

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require("path");
+
+const config = {
+    entry: ["./src/root.tsx"],
+    output: {
+        path: path.resolve(__dirname, "build"),
+        filename: "bundle.js"
+    },
+
+    resolve: {
+        extensions: [".ts", ".tsx", ".js"]
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                loader: "ts-loader",
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.(ttf|eot|woff|woff2)$/,
+                use: {
+                    loader: "file-loader",
+                    options: {
+                        name: "fonts/[name].[ext]",
+                    },
+                },
+            },
+        ],
+    },
+
+    devServer: {
+        overlay: true,
+    },
+};
+
+module.exports = config;
