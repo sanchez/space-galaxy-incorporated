918321dd7a21c0922b488c3707c67db6899a3028
sanchez <dfitz360@gmail.com>
2019-04-28 15:27:36 +1000

    Added a sun

--- src/three/field/floor.ts
+++ src/three/field/floor.ts
@@ -3,10 +3,10 @@ import { Renderable } from "../render";
 
 export default class Floor extends Renderable {
     onInit() {
-        const geo = new THREE.BoxGeometry(20, 0.1, 10);
+        const geo = new THREE.BoxGeometry(20, 0.1, 20);
         const color = 0xffffff;
         // const mat = new THREE.MeshBasicMaterial({ color });
-        const mat = new THREE.MeshPhongMaterial({ color });
+        const mat = new THREE.MeshPhysicalMaterial({ color, reflectivity: 1 });
         const cube = new THREE.Mesh(geo, mat);
         cube.receiveShadow = true;
 
--- src/three/world.ts
+++ src/three/world.ts
@@ -1,7 +1,7 @@
 import { Renderable } from "./render";
 import Field from "./field/field";
 import Player from "./players/Player";
-import { Vector3, Scene, PerspectiveCamera, BoxGeometry, MeshBasicMaterial, Mesh, PointLight } from "three";
+import { Vector3, Scene, PerspectiveCamera, BoxGeometry, MeshBasicMaterial, Mesh, PointLight, Sphere, SphereGeometry, MeshLambertMaterial } from "three";
 import Assets from "../api/Assets";
 import ShipController from "./players/ShipController";
 import WallController from "./players/WallController";
@@ -27,10 +27,14 @@ export default class World extends Renderable implements IUIElement {
         const field = new Field(this.scene);
         this.children.push(field);
 
-        const light = new PointLight(0xffffff, 0, 100, 2);
+        const light = new PointLight(0xffffaa, 0.2, 100, 2);
         light.position.set(25, 25, 25);
         light.castShadow = true;
         this.addElement("light", light);
+
+        const sun = new Mesh(new SphereGeometry(1), new MeshLambertMaterial({ color: 0xffffaa, emissive: 0xffffaa, emissiveIntensity: 10 }));
+        sun.position.set(light.position.x, light.position.y, light.position.z);
+        this.addElement("sun", sun);
 
         this.gameoverDiv = document.createElement("div");
         this.gameoverDiv.style.position = "fixed";
